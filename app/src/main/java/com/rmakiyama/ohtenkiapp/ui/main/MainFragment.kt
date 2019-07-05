a28f80fe4b522413164602d6db8abd1e7194271e
rmakiyama <rmakiyama.io@gmail.com>
2019-07-05 23:05:32 +0900

    必要なライブラリ追加

--- app/src/main/java/com/rmakiyama/ohtenkiapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/rmakiyama/ohtenkiapp/ui/main/MainFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import com.rmakiyama.ohtenkiapp.R
 
 class MainFragment : Fragment() {
@@ -14,19 +13,11 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
 }
